c2e874976f17a937d3d837a42607695bff6ed474
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-08-15 11:56:26 +0430

    added style to app.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,9 @@
-import classes from "./App.module.css"
+import classes from "./App.module.css";
 
 function App() {
   return (
     <div className={classes.App}>
-  
+      <div className={classes.Container}> hi </div>
     </div>
   );
 }
